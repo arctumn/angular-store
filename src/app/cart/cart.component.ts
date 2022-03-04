4e6156c084e8bb007f7bcb8bfa13a042792fa8e5
arctumn <pedro.lopes@latitudde.com>
2022-03-04 09:38:31 +0000

    form for purchase

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,12 +10,22 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
   
   items = this.cartService.getItems()
-  
+  checkoutForm = this.formBuilder.group(
+    {
+      name: '',
+      address: ''
+    }
+  )
   ngOnInit(): void {
   }
-
+  ngOnSubmit() {
+    this.items = this.cartService.clearCart()
+    console.warn('Your order has been submitted', this.checkoutForm.value)
+    this.checkoutForm.reset()
+  }
 }
